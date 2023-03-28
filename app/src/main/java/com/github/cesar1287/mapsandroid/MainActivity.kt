b780d41ac9645e7d45f9f2b3b8d28e984c73adf6
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-03-28 20:40:56 -0300

    google maps implementation

--- app/src/main/java/com/github/cesar1287/mapsandroid/MainActivity.kt
+++ app/src/main/java/com/github/cesar1287/mapsandroid/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.github.cesar1287.mapsandroid
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.github.cesar1287.mapsandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +16,73 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setUpListeners()
     }
+
+    private fun setUpListeners() {
+        binding.btIntentMap.setOnClickListener {
+            val navigation = "google.streetview:panoid=BQMXpbyPOUavEzjU1f4wbQ"
+
+            val navigationUri = Uri.parse(navigation)
+            val intent = Intent(Intent.ACTION_VIEW, navigationUri)
+
+            intent.setPackage("com.google.android.apps.maps")
+            startActivity(intent)
+
+//            val address = "Avenida Bezerra de Menezes, 1800, Fortaleza, Ceará, Brasil"
+//            val location = Uri.encode( address )
+//            val navigation = "google.navigation:q=$location"
+//
+//            val navigationUri = Uri.parse( navigation )
+//            val intent = Intent( Intent.ACTION_VIEW, navigationUri )
+//
+//            intent.setPackage( "com.google.android.apps.maps" )
+//            startActivity( intent )
+
+//            val query = "baladas"
+//            val latitudeLongitude = "-72.5565804,-54.662113"
+//            val geo = "geo:$latitudeLongitude?q=$query"
+//
+//            val geoUri = Uri.parse( geo )
+//            val intent = Intent( Intent.ACTION_VIEW, geoUri )
+//
+//            intent.setPackage( "com.google.android.apps.maps" )
+//            startActivity( intent )
+
+//            val query = "parques"
+//            val zoom = 20
+//            val geo = "geo:0,0?q=$query&z=$zoom"
+//
+//            val geoUri = Uri.parse( geo )
+//            val intent = Intent( Intent.ACTION_VIEW, geoUri )
+//
+//            intent.setPackage( "com.google.android.apps.maps" )
+//            startActivity( intent )
+
+//            val latitudeLongitude = "-23.5565804,-46.662113"
+//
+//            val zoom = 15
+//            val geo = "geo:$latitudeLongitude?z=$zoom"
+//
+//            val geoUri = Uri.parse( geo )
+//            val intent = Intent( Intent.ACTION_VIEW, geoUri )
+//
+//            intent.setPackage( "com.google.android.apps.maps" )
+
+            if( intent.resolveActivity( packageManager ) != null ) {
+                startActivity( intent )
+            }
+            else{
+                Toast.makeText(this, "Nenhum mapa instalado", Toast.LENGTH_LONG).show()
+            }
+
+
+        }
+
+        binding.btNativeMap.setOnClickListener {
+            startActivity(Intent(this, MapsActivity::class.java))
+        }
+    }
+
 }
